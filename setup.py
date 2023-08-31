28d9d89270e12540027bc642afc6b67a5724b609
Alexander <aandryukov@more.tv>
2023-08-31 14:47:34 +0500

    first test version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="common_fastapi",
+    description="My customized library for using FastAPI framework",
+    version="v1.0.0",
+    author="Alexander Andryukov",
+    author_email='andryukov@gmail.com',
+    install_requires=['fastapi==0.103.0']
+
+)
